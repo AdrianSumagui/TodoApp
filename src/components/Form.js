2040e3b8761f8193daf27dd3541fba00c7956b34
AdrianDEV <adriansumagui@outlook.com>
2021-09-30 13:35:12 +0200

    Reestructuración de los archivos que se van a trabajar y realización del formulario en Form.js

---
+++ src/components/Form.js
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+
+function Form(props) {
+
+const [input, setInput] = useState('');
+
+const handleChange = e => {
+
+    setInput(e.target.value);
+
+};
+
+const handleSubmit = e => {
+
+    e.preventDefault();
+
+    /*props.onSubmit({
+
+        id: Math.floor(Math.random() * 10000),
+        text: input
+
+    });*/
+
+    setInput('');
+
+};
+
+    return (
+
+        <>
+        
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Añade una tarea ^^"
+            value={input} name="text" className="todo-input"
+            onChange={handleChange}/>
+        </form>
+        
+        <button className="todo-button">Añadir tarea</button>
+        
+        </>
+
+    );
+
+}
+
+export default Form;
